93ee006e02347436e7b21fc70a1d201b0881d5ef
nirowu <rong.cs09@nycu.edu.tw>
2024-05-06 17:35:04 +0800

    modify user database

--- mariadb/schema.sql
+++ mariadb/schema.sql
@@ -14,8 +14,15 @@ CREATE TABLE IF NOT EXISTS User (
     user_id VARCHAR(36) PRIMARY KEY,
 	username VARCHAR(36) NOT NULL,
     password VARCHAR(60) NOT NULL,
+    real_name VARCHAR(36),
     cellphone VARCHAR(20),
     fb_account VARCHAR(100),
     email VARCHAR(100),
+    postcode VARCHAR (36),
     address VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS Product (
+    user_id VARCHAR(36) ,
+	product_id PRIMARY KEY;
 );
